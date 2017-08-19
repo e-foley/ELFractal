04587864ab8d2148ad7f3b4e9dad2e34f38a2af3
e-foley <efoley@calpoly.edu>
2017-08-18 20:16:44 -0700

    remove notice about image transfer ops

--- web/gallery.php
+++ web/gallery.php
@@ -1,6 +1,6 @@
 <?php
 require_once('./includes/db_connect.php');
-$notice = "Ed is currently relocating images previously hosted on Photobucket.  Thanks for your patience!";
+$notice = "";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
